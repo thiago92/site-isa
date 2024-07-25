76df6eba7cbe4f2eb4e9470c7050d3eae29f5e56
Thiago <thiago.dsouza1992@gmail.com>
2024-07-25 17:09:17 -0300

    Feat: novas alterações feitas no site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
-import { Component, HostListener, inject, TemplateRef  } from '@angular/core';
-
+import { Component, HostListener, inject, TemplateRef } from '@angular/core';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
-import { faX } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faPhone, faX } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
@@ -20,21 +17,28 @@ export class AppComponent {
   seconds!: number;
 
   private modalService = inject(NgbModal);
-	closeResult = '';
+  closeResult = '';
 
   faHouse = faHouse;
   faPhone = faPhone;
-  faX = faX
+  faX = faX;
+
+  phoneNumber: string = '554191310319';
+  message: string = 'Olá, gostaria de saber mais sobre os projetos da Isa Bandeira.';
 
   constructor() { }
 
   ngOnInit(): void {
-    
   }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    this.timer();
+    const navbar = document.querySelector('.navbar') as HTMLElement;
+    if (window.scrollY > 50) { // Ajuste esse valor conforme necessário
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
   }
 
   timer() {
@@ -64,31 +68,34 @@ export class AppComponent {
       this.hours = remainingHours;
     }, 1000);
   }
-  
 
   open(content: TemplateRef<any>) {
-		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
-			(result) => {
-				this.closeResult = `Closed with: ${result}`;
-			},
-			(reason) => {
-				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-			},
-		);
-	}
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      (result) => {
+        this.closeResult = `Closed with: ${result}`;
+      },
+      (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      },
+    );
+  }
 
-	private getDismissReason(reason: any): string {
-		switch (reason) {
-			case ModalDismissReasons.ESC:
-				return 'by pressing ESC';
-			case ModalDismissReasons.BACKDROP_CLICK:
-				return 'by clicking on a backdrop';
-			default:
-				return `with: ${reason}`;
-		}
-	}
+  private getDismissReason(reason: any): string {
+    switch (reason) {
+      case ModalDismissReasons.ESC:
+        return 'by pressing ESC';
+      case ModalDismissReasons.BACKDROP_CLICK:
+        return 'by clicking on a backdrop';
+      default:
+        return `with: ${reason}`;
+    }
+  }
 
-  goToLink(url: string){
+  goToLink(url: string) {
     window.open(url, "_blank");
+  }
+
+  getWhatsAppLink(): string {
+    return `https://api.whatsapp.com/send?phone=${this.phoneNumber}&text=${encodeURIComponent(this.message)}`;
   }
 }
